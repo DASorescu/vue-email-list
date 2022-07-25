27b96cd4a81fab93cf4a921d14c088de6e226640
github_DASorescu <srsdln97@gmail.com>
2022-07-25 15:17:47 +0200

    completed-exercise

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const app = new Vue({
+    el: "#root",
+    data: {
+      emailList: [],
+    
+    },
+    created() {
+      for (let i = 0; i < 10; i++) {
+           axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((res) => {
+            this.emailList.push(res.data.response);
+          });
+        }
+    },
+  });
